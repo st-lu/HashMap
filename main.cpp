47707b115a767f89c9783b0625124f452ce0fbe4
Luana <62220895+st-lu@users.noreply.github.com>
2020-05-17 18:52:45 +0300

    added example of user-inputted hash function, tweaked the default hash function a little, fixed copy constr and = operator again

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 #include "HashMap.h"
 using namespace std;
+
+class Hashing {
+    int size;
+public:
+    explicit Hashing(int size = 1111){
+        this->size = size;
+    }
+    int operator()(const string& key) const{
+        int sum = 0;
+        for(auto ch : key){
+            sum += int(ch);
+        }
+        return sum;
+    }
+};
 
 int main() {
     HashMap<int, int> H;
@@ -16,11 +31,19 @@ int main() {
     assert(H[3] == 4);
     assert(H.getNoOfKeys() == 2);
     H.remove(3, 4);
+    HashMap<int, int> h(H);
+    cout << h;
     assert(H.getValuesByKey(3).size() == 1 && *H.getValuesByKey(3).begin() == 5);
     assert(H.search(976, 10));
     H.remove(3);
     assert(!H.search(3));
-    HashMap<int, int> h(H);
-    cout << h;
+
+    HashMap<string, int, Hashing> StringHash;
+    StringHash.add("hello", 3);
+    StringHash.add("world", 90);
+    cout << StringHash;
+    assert(*StringHash.getValuesByKey("hello").begin() == 3);
+    assert(StringHash.search("world"));
+    assert(StringHash["hello"] == 3);
     return 0;
 }
